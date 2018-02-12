347d260c82ecd00caa86dfaf3e6bb3a1a7468f56
csquilla567 <36014412+csquilla567@users.noreply.github.com>
2018-02-12 10:18:57 -0500

    Add files via upload

---
+++ gitHubAPI.py
@@ -0,0 +1,36 @@
+'''
+Created on Feb 11, 2018
+
+@author: Caroline Squillante
+'''
+
+import requests
+import json
+
+def repoNum(userId):
+    
+    r = requests.get("https://api.github.com/users/" + userId + "/repos")
+    repos = []
+    repoData = json.loads(r.text)
+    
+    for info in repoData:
+        repos += [info.get('name')]
+    return repos
+    
+def commitNum(userId, repoName):
+    r = requests.get('https://api.github.com/repos/' + userId + '/' + repoName + '/commits')
+    commitData = json.loads(r.text)
+    return len(commitData)
+
+if __name__ == "__main__":
+    user = input("Enter a Github username to view their repositories ")
+    print("User: " + user)
+
+    repos = repoNum(user)
+
+    for r in repos:
+        print("Repo: " + r + " Number of Commits: " + str(commitNum(user,r)))
+        
+        
+        
+        
