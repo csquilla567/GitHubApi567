d3b0079acd2c1c4e600179a944f086abc15383ae
csquilla567 <36014412+csquilla567@users.noreply.github.com>
2018-02-12 11:33:35 -0500

    more test cases

--- TestGitHubApi.py
+++ TestGitHubApi.py
@@ -1,33 +1,53 @@
 '''
-Created on Feb 11, 2018
+Created on Feb 12, 2018
 
 @author: Caroline Squillante
 '''
+import unittest
+from gitHubAPI import repoNames, commitNum
 
-import requests
-import json
-
-def repoNames(userId):
+class TestGithub(unittest.TestCase):
     
-    r = requests.get("https://api.github.com/users/" + userId + "/repos")
-    repos = []
-    repoData = json.loads(r.text)
-    
-    for info in repoData:
-        repos += [info.get('name')]
-    return repos
-    
-def commitNum(userId, repoName):
-    
-    r = requests.get('https://api.github.com/repos/' + userId + '/' + repoName + '/commits')
-    commitData = json.loads(r.text)
-    return len(commitData)
+    def testRepo1(self):
+        allRepos = repoNames('csquillz')
+        self.assertGreaterEqual(len(allRepos), 1)
+        self.assertIn('SSW',allRepos)
 
-if __name__ == "__main__":
-    user = input("Enter a Github username: ")
-    print("User: " + user)
-
-    repos = repoNames(user)
-
-    for r in repos:
-        print("Repo: " + r + " Number of Commits: " + str(commitNum(user,r))) 
+    def testRepo2(self):
+        allRepos = repoNames('csquilla567')
+        self.assertGreaterEqual(len(allRepos), 2)
+        self.assertIn('Triangle567',allRepos)
+        self.assertIn('GitHubApi567',allRepos)
+    
+    def testRepo3(self):
+        allRepos = repoNames('karuaan')
+        self.assertGreaterEqual(len(allRepos), 7)
+        self.assertIn('ssw555CKMM2018Spring',allRepos)
+        self.assertIn('CS-546-Web-Programming',allRepos)
+        self.assertIn('karuaan.github.io',allRepos)
+        self.assertIn('Android-Projects',allRepos)
+        self.assertIn('Systems-Programming-',allRepos)
+        self.assertIn('MusicMeld',allRepos)
+        self.assertIn('Ratemycourse.com',allRepos)
+            
+    def testNumCommits1(self):
+        self.assertGreaterEqual(commitNum('csquillz','SSW'),30)
+        
+    def testNumCommits2(self):
+        self.assertGreaterEqual(commitNum('csquilla567','Triangle567'),4)
+        
+    def testNumCommits3(self):
+        self.assertGreaterEqual(commitNum('karuaan','ssw555CKMM2018Spring'),23)
+        self.assertGreaterEqual(commitNum('karuaan','CS-546-Web-Programming'),1)
+        self.assertGreaterEqual(commitNum('karuaan','karuaan.github.io'),2)
+        self.assertGreaterEqual(commitNum('karuaan','Android-Projects'),2)
+        self.assertGreaterEqual(commitNum('karuaan','Systems-Programming-'),18)
+        self.assertGreaterEqual(commitNum('karuaan','MusicMeld'),6)
+        self.assertGreaterEqual(commitNum('karuaan','Ratemycourse.com'),6)
+        
+    #def testGitHubApi(self): 
+        
+           
+        
+        
+        
